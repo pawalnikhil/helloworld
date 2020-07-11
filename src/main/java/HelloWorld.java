bc7f5fb019fe3780eaee301edb698cf9163747b4
Nikhil Pawal <nikhipaw@amdocs.com>
2020-07-11 16:23:50 +0530

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World");
+    }
+}
