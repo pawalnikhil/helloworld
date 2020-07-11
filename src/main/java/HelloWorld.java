8001739e738e77fc5d8730ddeacf2be15d5e82a8
Nikhil Pawal <nikhipaw@amdocs.com>
2020-07-11 17:04:58 +0530

    added 1 line

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,5 +5,6 @@ import java.io.StringWriter;
 public class HelloWorld {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
