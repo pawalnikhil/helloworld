9c4e310aaec18f4fec80eaf480e1a4f21b5673cb
pawalnikhil <pawal.nikhil@gmail.com>
2020-07-11 17:06:10 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
